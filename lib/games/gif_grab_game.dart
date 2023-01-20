c8961f06955194545f5da9bab2908e86711308ab
IagoAntunes <iagoantunes.f@gmail.com>
2023-01-20 15:55:53 -0300

    joystick

--- lib/games/gif_grab_game.dart
+++ lib/games/gif_grab_game.dart
@@ -1,13 +1,16 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:santagame/components/background_component.dart';
 import 'package:santagame/components/santa_component.dart';
+import 'package:santagame/inputs/joystick.dart';
 
-class GiftGrabGame extends FlameGame {
+class GiftGrabGame extends FlameGame with HasDraggables {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
     add(backgroundComponent());
     add(SantaComponent());
+    add(joystick);
   }
 }
